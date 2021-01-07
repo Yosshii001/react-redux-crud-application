32e91f53b21644331f2da3222d7b0532042c33d3
yoshitanijunichi <jun.and.guri@gmail.com>
2021-01-08 02:37:45 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import React from 'react';
 function App() {
-  return <h1>edit src/App.js and save to reload.</h1>;
+  return (
+  <React.Fragment>
+    <h1>edit src/App.js and save to reload.</h1>
+  </React.Fragment>
+  );
 }
 
 export default App;
