01541f0ebe2c9575e3ac08642bc7eeb64bbe7be8
yoshitanijunichi <jun.and.guri@gmail.com>
2021-01-08 02:58:08 +0900

    create class components and function components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ function App() {
   return (
   <React.Fragment>
     <h1>edit src/App.js and save to reload.</h1>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
   </React.Fragment>
   );
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
